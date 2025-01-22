11e8747a1b598dde8aabe2a5bd1ba400fbde7453
MRBOX <olamigokeadebayo90@gmail.com>
2025-01-22 15:45:50 +0100

    Hero page

--- frontend/src/components/LatestCollection.jsx
+++ frontend/src/components/LatestCollection.jsx
@@ -1,10 +1,17 @@
-import React, { useContext } from 'react'
+import React, { useContext, useEffect, useState } from 'react'
 import { ShopContext } from '../context/ShopContext'
 import Title from './Title';
+import ProductItem from './ProductItem';
 
 const LatestCollection = () => {
 
     const { products } = useContext(ShopContext);
+
+    const [lastestProducts, setLatestProducts] = useState([]);
+
+    useEffect(() =>{
+        setLatestProducts(products.slice(0,10));
+    }, [])
 
     
 
@@ -12,6 +19,19 @@ const LatestCollection = () => {
     <div className='my-10'>
         <div className='text-center py-8 text-3xl'>
             <Title text1={"LASTEST"} text2={'COLLECTION'} />
+            <p className='w-3/4 m-auto text-xs sm:text-sm md:text-base text-gray-600'>o quos soluta corrupti voluptatibus temporibus expedita magni modi repellat quod nihil.
+            </p>
+        </div>
+
+        {/* Rendering Products */}
+
+        <div className='grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-col-5
+         gap-4 gap-y-6'>
+            {
+                lastestProducts.map((item, index) => (
+                    <ProductItem key={index} id={item.id} image={item.image} name={item.name} price={item.price}/>
+                ))
+            }
         </div>
     </div>
   )
